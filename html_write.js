b90673ad30ed078f351c5fc80d763bf324306f2f
rihitosan <76610702+rihitosan@users.noreply.github.com>
2022-04-16 23:16:44 +0900

    Add files via upload

---
+++ html_write.js
@@ -0,0 +1,80 @@
+var header_write = document.getElementsByClassName("header")[0]
+
+header_write.innerHTML = (function() {/*
+<header>
+	<a href="/"><img src="https://uniant.net/assets/image/uniantlogo.png" alt="" height="30px"></a>
+    <div class="menunormal">
+        <nav>
+            <ul>
+                <li><a href="https://uniant.net/about/">About</a></li>
+                <li><a href="https://uniant.net/news/">News</a></li>
+                <li><a href="https://blog.uniant.net/">Blog</a></li>
+                <li><a href="https://support.uniant.net/">Contact</a></li>
+            </ul>
+        </nav>
+    </div>
+    <div class="menuresponsive">
+        <div class="menulines" id="hamburgerbutton" onclick="hamburgerButtonClick()">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+        <div class="responsivemenucontent">
+            <nav>
+                <ul>
+                    <li><a href="https://uniant.net/about/">About</a></li>
+                    <li><a href="https://uniant.net/news/">News</a></li>
+                    <li><a href="https://blog.uniant.net/">Blog</a></li>
+                    <li><a href="https://support.uniant.net/">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
+*/}).toString().match(/[^]*\/\*([^]*)\*\/\}$/)[1].replace(/\n|\r/g, "");
+
+
+var footer_write = document.getElementsByClassName("footer")[0]
+
+footer_write.innerHTML = (function() {/*
+<img src="https://uniant.net/assets/image/footer.svg" alt="">
+<footer>
+	<div class="footercontents">
+		<div class="leftcontent">
+			<h1 class="uniant">Uniant</h1>
+			<span class="footerslogan">New Future, New Technology.</span>
+			<hr class="footerline">
+			<div class="sites">
+				<div class="sitesfooterline">
+					<span>Web:</span>
+					<a href="https://uniant.net" target="_blank" rel="noopener noreferrer"><span>https://uniant.net/</span></a>
+				</div>
+				<div class="sitesfooterline">
+					<span>Twitter:</span>
+					<a href="https://twitter.com/UniantJapan" target="_blank" rel="noopener noreferrer"><span>@UniantJapan</span></a>
+				</div>
+				<div class="sitesfooterline">
+					<span>GitHub:</span>
+					<a href="https://github.com/Uniant" target="_blank" rel="noopener noreferrer"><span>Uniant</span></a>
+				</div>
+				<div class="sitesfooterline">
+					<span>Discord:</span>
+					<a href="https://discord.gg/GYUKdPk" target="_blank" rel="noopener noreferrer"><span>https://discord.gg/GYUKdPk</span></a>
+				</div>
+			</div>
+			<div class="copyright">
+				<span>Designed by Yobibyte /<br>&copy; 2021 Uniant</span>
+			</div>
+		</div>
+		<div class="centermenu">
+			<span>About</span>
+			<div class="menucontent">
+				<a href="https://uniant.net/about/"><p>About Uniant</p></a>
+				<a href="https://uniant.net/projects/"><p>Projects</p></a>
+				<a href="https://uniant.net/join/"><p>Join</p></a>
+				<a href="https://support.uniant.net/"><p>Contact</p></a>
+			</div>
+		</div>
+	</div>
+</footer>
+*/}).toString().match(/[^]*\/\*([^]*)\*\/\}$/)[1].replace(/\n|\r/g, "");
---
+++ projects/bridge/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300&family=Noto+Sans+JP:wght@400&display=swap" rel="stylesheet">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width">
+    <meta name="description" content="Linux のコマンドを Windows で利用可能にする Uniant のプロジェクト「Bridge」についてご紹介します。">
+    <link rel="icon" href="/favicon.png">
+	<title>Bridge - Uniant</title>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@700&family=Noto+Sans+JP:wght@300;500&display=swap" rel="stylesheet">
+	<link href="../../assets/css/style.css" rel="stylesheet" type="text/css" />
+	<link href="../../assets/css/page/projects.css" rel="stylesheet" type="text/css" />
+    <meta name="twitter:card" content="summary">
+	<meta name="twitter:site" content="@UniantJapan">
+	<meta property="og:url" content="https://uniant.net/projects/bridge/">
+	<meta property="og:title" content="Bridge - Uniant">
+	<meta property="og:description" content="Linux のコマンドを Windows で利用可能にするアプリ「Bridge」のページです。">
+	<meta property="og:image" content="https://uniant.net/card.png">
+</head>
+<body>
+	<div class="header">
+        <?php include('../../header.php') ?>
+    </div>
+        <main>
+            <div class="top">
+                <img src="../../assets/image/projects/bridge/bridge.jpg" alt="">
+                <div class="toptext">
+                    <span class="toptitle">Bridge</span>
+                    <hr class="topline">
+                    <span class="topcatch">コマンドはストレスフリーで。</span>
+                </div>
+            </div>
+            <div class="description topmargin">
+                <div class="box">
+                    <div class="boxleft">
+                        <div class="texts">
+                            <span class="title">コマンドを入力するだけ。</span>
+                            <div class="desc">
+                                <span>変換したいコマンドを入力して実行ボタンを押すだけ。<br>Windows コマンドはそのまま、Linux コマンドは<br>変換されて実行されます。</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="boxright">
+                        <img src="../../assets/image/projects/bridge/typing.svg" alt="" style="width: 50%;">
+                    </div>
+                </div>
+                <div class="download">
+                    <span class="title">今すぐストレスフリーに。</span>
+                    <div class="downloadbutton">
+                        <a href="./bridge1a.zip" download><img src="../../assets/image/projects/download.svg" alt=""><span>Download for Windows</span></a>
+                    </div>
+                </div>
+            </div>
+        </main>
+		<div class="footer">
+			<?php include('../../footer.php') ?>
+		</div>
+    <script src="../../assets/js/script.js" type="text/javascript"></script>
+  </body>
+</html>
